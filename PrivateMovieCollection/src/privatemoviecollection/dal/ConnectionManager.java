4c41aaa83f0bbfad17d6fb898599955937df6828
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-01-09 10:39:52 +0100

    connectionmanager done

--- PrivateMovieCollection/src/privatemoviecollection/dal/ConnectionManager.java
+++ PrivateMovieCollection/src/privatemoviecollection/dal/ConnectionManager.java
@@ -5,10 +5,37 @@
  */
 package privatemoviecollection.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+
+
+
 /**
- *
+ *@ Class for constructing a connector we can use when we want to connect to our DB throughout our application.
  * @author pmj
  */
 public class ConnectionManager {
-    
+    private final SQLServerDataSource ds = new SQLServerDataSource(); 
+
+    /**
+     * Connection constructor with connector parameters.
+     */
+public ConnectionManager() {
+        ds.setDatabaseName("PrivateMovieCollection");
+        ds.setUser("CS2017A_28");
+        ds.setPassword("Ellap060174");
+        ds.setServerName("EASV-DB2");
+        ds.setPortNumber(1433);
+    }
+
+
+    /**
+     * @return our connection
+     * @throws SQLServerException
+     */
+public Connection getConnection() throws SQLServerException
+    {
+        return ds.getConnection();
+    }
 }
