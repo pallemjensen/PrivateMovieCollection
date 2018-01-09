508f2d1de4eea9da59b1ce42f5ca2eeff22dfc7a
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-01-09 13:16:58 +0100

    model imports and lists

--- PrivateMovieCollection/src/privatemoviecollection/gui/PMCModel.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/PMCModel.java
@@ -5,10 +5,23 @@
  */
 package privatemoviecollection.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import privatemoviecollection.be.category;
+import privatemoviecollection.be.movie;
+import privatemoviecollection.bll.BLLManager;
 /**
  *
  * @author pmj
  */
 public class PMCModel {
+    
+    private final BLLManager bllManager = new BLLManager();
+
+    private final ObservableList<movie> movies
+            = FXCollections.observableArrayList();
+    
+    private final ObservableList<category> categories
+            = FXCollections.observableArrayList();
     
 }
