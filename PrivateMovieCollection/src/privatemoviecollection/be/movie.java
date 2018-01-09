009d62b30d07364195b0dbc82c55beeec5870ece
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-01-09 12:03:05 +0100

    be movies

--- PrivateMovieCollection/src/privatemoviecollection/be/movie.java
+++ PrivateMovieCollection/src/privatemoviecollection/be/movie.java
@@ -5,10 +5,73 @@
  */
 package privatemoviecollection.be;
 
+import java.util.Date;
+
 /**
  *
  * @author pmj
  */
 public class movie {
+private int id;
+private String name;
+double imdbRating;
+double privateRating;
+String fileLink;
+long lastView;
+
     
+public movie(int id, String name, double imdbRating, double privateRating, String fileLink, Date lastView)
+{    
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public double getPrivateRating() {
+        return privateRating;
+    }
+
+    public void setPrivateRating(double privateRating) {
+        this.privateRating = privateRating;
+    }
+
+    public String getFileLink() {
+        return fileLink;
+    }
+
+    public void setFileLink(String fileLink) {
+        this.fileLink = fileLink;
+    }
+
+    public long getLastView() {
+        return lastView;
+    }
+
+    public void setLastView(Long lastView) {
+        this.lastView = lastView;
+    }
+
+
+
 }
