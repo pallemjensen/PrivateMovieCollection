4d40b64a6a59ff6d5b35c65404cac88c804330f1
Bomholt <33052411+Bomholt@users.noreply.github.com>
2018-01-10 09:14:47 +0100

    .

--- PrivateMovieCollection/src/privatemoviecollection/be/PMCException.java
+++ PrivateMovieCollection/src/privatemoviecollection/be/PMCException.java
@@ -6,22 +6,38 @@
 package privatemoviecollection.be;
 
 /**
- *
+ * 
  * @author Sven
  */
 public class PMCException extends Exception {
 
+    /**
+     *
+     */
     public PMCException() {
     }
 
+    /**
+     *
+     * @param string
+     */
     public PMCException(String string) {
         super(string);
     }
 
+    /**
+     *
+     * @param string
+     * @param thrwbl
+     */
     public PMCException(String string, Throwable thrwbl) {
         super(string, thrwbl);
     }
 
+    /**
+     *
+     * @param thrwbl
+     */
     public PMCException(Throwable thrwbl) {
         super(thrwbl);
     }
