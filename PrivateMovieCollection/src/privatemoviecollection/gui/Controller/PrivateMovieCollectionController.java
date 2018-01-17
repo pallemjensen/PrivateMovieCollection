2a791b6df5625c9f5afebbaa236a737840cee790
Bomholt <33052411+Bomholt@users.noreply.github.com>
2018-01-17 11:50:32 +0100

    refactoring

--- PrivateMovieCollection/src/privatemoviecollection/gui/Controller/PrivateMovieCollectionController.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/Controller/PrivateMovieCollectionController.java
@@ -233,9 +233,10 @@ public class PrivateMovieCollectionController implements Initializable {
     @FXML
     private void btnFilterOnImdbRating(ActionEvent event) {
         try {
-            Double filter = Double.valueOf(txtImdbFilter.getText());
-            ObservableList<Movie> filteredMovies = pmcModel.filterOnRating(filter);
-            TVMovies.setItems(filteredMovies);
+//            Double filter = Double.valueOf(txtImdbFilter.getText());
+//            ObservableList<Movie> filteredMovies = pmcModel.filterOnRating(filter);
+//            TVMovies.setItems(filteredMovies);
+            TVMovies.setItems(pmcModel.filterOnRating(Double.valueOf(txtImdbFilter.getText())));
         } catch (PMCException ex) {
             exceptionHandler(ex);
         }
