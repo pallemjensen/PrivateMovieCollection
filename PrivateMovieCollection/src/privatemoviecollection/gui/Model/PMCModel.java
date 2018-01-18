eb3a8a7383041d40b6b973700e09f106af12c3b3
Bomholt <33052411+Bomholt@users.noreply.github.com>
2018-01-19 00:49:40 +0100

    .

--- PrivateMovieCollection/src/privatemoviecollection/gui/Model/PMCModel.java
+++ PrivateMovieCollection/src/privatemoviecollection/gui/Model/PMCModel.java
@@ -5,7 +5,6 @@
  */
 package privatemoviecollection.gui.Model;
 
-import java.util.ArrayList;
 import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
